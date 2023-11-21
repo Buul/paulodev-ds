bcd81c8701c3eabd410670977a14ba1bb07012d9
paulosilvacliqx <paulo.silva@cliqx.com.br>
2023-11-21 14:07:17 -0300

    docs(storybook): added structural storybook files and button stories

---
+++ lib/components/Button/Button.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta } from '@storybook/react';
+import { Button } from '.';
+
+const meta: Meta = {
+  title: '🧩 Components/Forms/Button',
+  component: Button,
+  argTypes: {},
+  parameters: {
+    controls: { expanded: true },
+  },
+};
+
+export default meta;
+
+const Template = () => {
+  return <Button />;
+};
+
+export const All = Template.bind({});
