095275d65e15ee8774249989541038dea3ebc3d8
paulosilvacliqx <paulo.silva@cliqx.com.br>
2023-11-21 14:08:13 -0300

    feat(components): added Button and Input component

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-unresolved */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Button, Input } from '../lib/main';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Button />
+    <Input />
+  </React.StrictMode>,
+)
